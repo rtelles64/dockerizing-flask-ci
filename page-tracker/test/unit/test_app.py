52008229edf13a48a0a8f2ec34a17b6c44ddc8f1
Roy Telles <rtelles64@gmail.com>
2023-07-23 15:19:04 -0700

    Add: test connection method

--- page-tracker/test/unit/test_app.py
+++ page-tracker/test/unit/test_app.py
@@ -2,6 +2,7 @@ import pytest
 import unittest.mock
 
 from page_tracker.app import app
+from redis import ConnectionError
 
 
 @pytest.fixture
@@ -21,3 +22,16 @@ def test_should_call_redis_incr(mock_redis, http_client):
     assert response.status_code == 200
     assert response.text == "This page has been seen 5 times."
     mock_redis.return_value.incr.assert_called_once_with("page_views")
+
+
+@unittest.mock.patch("page_tracker.app.redis")
+def test_should_handle_redis_connection_error(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.side_effect = ConnectionError
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 500
+    assert response.text == "Sorry, something went wrong \N{pensive face}"

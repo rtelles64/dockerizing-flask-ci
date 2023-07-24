ca35ce2638ac9371e5287fdd84febb9fbaefa967
Roy Telles <rtelles64@gmail.com>
2023-07-23 21:59:53 -0700

    Update: Comment to indicate to pylint to ignore error; Add: name-main idiom

--- page-tracker/src/page_tracker/app.py
+++ page-tracker/src/page_tracker/app.py
@@ -7,12 +7,12 @@ from redis import Redis, RedisError
 app = Flask(__name__)
 
 
-@app.get('/')
+@app.get("/")
 def index():
     try:
         page_views = redis().incr("page_views")
     except RedisError:
-        app.logger.exception("Redis error")
+        app.logger.exception("Redis error")  # pylint: disable=E1101
         return "Sorry, something went wrong \N{pensive face}", 500
     else:
         return f"This page has been seen {page_views} times."
@@ -23,3 +23,7 @@ def redis():
     # If the REDIS_URL isn't configured, the default database on localhost is
     # used instead.
     return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379/0"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
